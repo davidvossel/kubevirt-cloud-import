46559572b2bdad76dc542b745d8de3bd691c205f
David Vossel <davidvossel@gmail.com>
2021-08-26 17:15:08 -0400

    Wire up aws client and aws go sdk
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- cmd/virt-image-importer/virt-image-importer.go
+++ cmd/virt-image-importer/virt-image-importer.go
@@ -1,7 +1,81 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+)
+
+var (
+	region string
+	amiId  string
+)
+
+func init() {
+	flag.StringVar(&region, "region", "", "The AWS region.")
+	flag.StringVar(&amiId, "ami-id", "", "The ID of the ami to import")
+}
 
 func main() {
-	fmt.Println("vim-go")
+	flag.Parse()
+
+	if amiId == "" {
+		log.Fatalf("--ami-id is required")
+	}
+
+	// Load the SDK's configuration from environment and shared config, and
+	// create the ec2Client with this.
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatalf("failed to load SDK configuration, %v", err)
+	}
+
+	if region == "" {
+		region = cfg.Region
+	}
+
+	ec2Client := ec2.NewFromConfig(cfg)
+	stsClient := sts.NewFromConfig(cfg)
+
+	params := &ec2.DescribeImagesInput{
+		ImageIds: []string{amiId},
+	}
+
+	amiListOutput, err := ec2Client.DescribeImages(context.Background(), params, func(o *ec2.Options) {
+		o.Region = region
+	})
+	if err != nil {
+		log.Fatalf("Failed to retrieve ami list: %v", err)
+	}
+
+	identityOutput, err := stsClient.GetCallerIdentity(context.Background(), &sts.GetCallerIdentityInput{}, func(o *sts.Options) {
+		o.Region = region
+	})
+
+	fmt.Printf("images found: %d\n", len(amiListOutput.Images))
+	fmt.Printf("metadata: %v\n", amiListOutput.ResultMetadata)
+	for _, image := range amiListOutput.Images {
+		if image.ImageId != nil {
+			log.Printf("image: %s\n", *image.ImageId)
+		}
+
+		if image.OwnerId != nil {
+			log.Printf("image owner: %s\n", *image.OwnerId)
+		}
+	}
+
+	if identityOutput.Account != nil {
+		fmt.Printf("My account id: %s\n", *identityOutput.Account)
+	}
+
+	// TODO copy if
+	// * ami account doesn't equal current account
+	// * regions don't match
+	// Use name as unique id, name of AMI copy is immutable which means we can use it to determine
+	// if the copy already took place
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module kubevirt.io/kubevirt-cloud-import
 
 go 1.15
+
+require (
+	github.com/aws/aws-sdk-go-v2/config v1.6.1
+	github.com/aws/aws-sdk-go-v2/service/ec2 v1.14.0
+	github.com/aws/aws-sdk-go-v2/service/sts v1.6.2
+)
