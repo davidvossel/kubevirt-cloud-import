eae50a7654bf1187c6faec4ea997dcf32178d8d4
David Vossel <davidvossel@gmail.com>
2021-08-27 16:51:35 -0400

    Add logic for ami export to s3 bucket
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- cmd/virt-image-importer/virt-image-importer.go
+++ cmd/virt-image-importer/virt-image-importer.go
@@ -10,14 +10,20 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
 )
 
 type client struct {
 	ec2Client *ec2.Client
 	stsClient *sts.Client
+	s3Client  *s3.Client
 	region    string
 }
+
+const (
+	ExportImageFormat = "vmdk"
+)
 
 func NewClient(region string) (*client, error) {
 
@@ -34,10 +40,12 @@ func NewClient(region string) (*client, error) {
 
 	ec2Client := ec2.NewFromConfig(cfg)
 	stsClient := sts.NewFromConfig(cfg)
+	s3Client := s3.NewFromConfig(cfg)
 
 	return &client{
 		ec2Client: ec2Client,
 		stsClient: stsClient,
+		s3Client:  s3Client,
 		region:    region,
 	}, nil
 }
@@ -62,6 +70,87 @@ func (c *client) FindGlobalImageById(amiId string) (*types.Image, error) {
 	image := amiListOutput.Images[0]
 
 	return &image, nil
+}
+
+func (c *client) ExportImage(amiId string, s3Bucket string, s3Prefix string, imageFormat string) (string, error) {
+
+	if imageFormat != "vmdk" {
+		return "", fmt.Errorf("Image format %s is unsupported, only image format 'vmdk' is supported", imageFormat)
+	}
+
+	description := fmt.Sprintf("Exporting ami %s for import into KubeVirt cluster", amiId)
+	params := &ec2.ExportImageInput{
+		DiskImageFormat: types.DiskImageFormatVmdk,
+		ImageId:         &amiId,
+		S3ExportLocation: &types.ExportTaskS3LocationRequest{
+			S3Bucket: &s3Bucket,
+			S3Prefix: &s3Prefix,
+		},
+		Description: &description,
+	}
+
+	amiExportOutput, err := c.ec2Client.ExportImage(context.Background(), params, func(o *ec2.Options) {
+		o.Region = c.region
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	if amiExportOutput.ExportImageTaskId == nil {
+		return "", fmt.Errorf("No export task id provided for ExportImage job. Unable to track export")
+	}
+
+	return *amiExportOutput.ExportImageTaskId, nil
+}
+
+func (c *client) GetExportTaskStatus(exportTaskId string) (string, error) {
+	params := &ec2.DescribeExportImageTasksInput{
+		ExportImageTaskIds: []string{exportTaskId},
+	}
+
+	exportTaskOutput, err := c.ec2Client.DescribeExportImageTasks(context.Background(), params, func(o *ec2.Options) {
+		o.Region = c.region
+	})
+	if err != nil {
+		return "", err
+	}
+
+	if len(exportTaskOutput.ExportImageTasks) == 0 || exportTaskOutput.ExportImageTasks[0].Status == nil {
+		return "", fmt.Errorf("Export task id %d not found. Unable to track export", exportTaskId)
+	}
+
+	return *exportTaskOutput.ExportImageTasks[0].Status, nil
+}
+
+func (c *client) WaitForExportImageCompletion(taskId string, timeout time.Duration) error {
+	ticker := time.NewTicker(timeout).C
+	pollTicker := time.NewTicker(time.Second * 15).C
+
+	log.Printf("Polling task id %s to determine if it is completed", taskId)
+	status, _ := c.GetExportTaskStatus(taskId)
+	if status == "completed" {
+		return nil
+	}
+
+	// if not available, poll until available or timeout is hit
+	for {
+		select {
+		case <-ticker:
+			return fmt.Errorf("timed out waiting for task id %s to become complete", taskId)
+		case <-pollTicker:
+			log.Printf("Polling task id %s to determine if it is completed", taskId)
+
+			status, err := c.GetExportTaskStatus(taskId)
+			if err != nil {
+				log.Printf("err encountered looking up task id %s: %v", taskId, err)
+				continue
+			} else if status == "completed" {
+				log.Printf("Task id %s completed", taskId)
+				return nil
+			}
+		}
+	}
 }
 
 func (c *client) FindImageByName(amiName string, accountId string) (*types.Image, bool, error) {
@@ -93,6 +182,10 @@ func (c *client) FindImageByName(amiName string, accountId string) (*types.Image
 
 func (c *client) CopyImageName(amiId string) string {
 	return fmt.Sprintf("kubevirt-export-automation-copy-%s", amiId)
+}
+
+func (c *client) ExpectedImageS3Key(amiId string, imageFormat string) string {
+	return fmt.Sprintf("export-%s.%s", amiId, imageFormat)
 }
 
 func (c *client) CopyImage(amiId string, amiCopyName string) (string, error) {
@@ -144,6 +237,31 @@ func (c *client) IsImageAvailable(amiId string) (bool, error) {
 	return false, nil
 }
 
+func (c *client) IsImageExported(s3Bucket string, s3Prefix string, amiId string, imageFormat string) (bool, error) {
+
+	fullPrefix := s3Prefix + c.ExpectedImageS3Key(amiId, imageFormat)
+
+	log.Printf("Searching for export in bucket %s with prefix %s", s3Bucket, fullPrefix)
+
+	params := &s3.ListObjectsInput{
+		Bucket: &s3Bucket,
+		Prefix: &fullPrefix,
+	}
+	listOutput, err := c.s3Client.ListObjects(context.Background(), params, func(o *s3.Options) {
+		o.Region = c.region
+	})
+
+	if err != nil {
+		return false, err
+	} else if len(listOutput.Contents) == 0 {
+		return false, nil
+	} else if len(listOutput.Contents) > 1 {
+		return false, fmt.Errorf("found multiple matching s3 exports for ami %s", amiId)
+	}
+
+	return true, nil
+}
+
 func (c *client) WaitForImageToBecomeAvailable(amiId string, timeout time.Duration) error {
 	ticker := time.NewTicker(timeout).C
 	pollTicker := time.NewTicker(time.Second * 15).C
@@ -177,13 +295,19 @@ func main() {
 
 	var region string
 	var amiId string
+	var s3Bucket string
+	var s3Prefix string
 
 	flag.StringVar(&region, "region", "", "The AWS region the AMI resides in. NOTE: if the AMI is shared from another account, a copy of the AMI will be created in the client's account in order to import to KubeVirt")
 	flag.StringVar(&amiId, "ami-id", "", "The ID of the ami to import")
+	flag.StringVar(&s3Bucket, "s3-bucket", "", "The s3 bucket to use to store and deliver the AMI into kubevirt")
+	flag.StringVar(&s3Prefix, "s3-prefix", "kubevirt-image-exports/", "The s3 directory prefix to use to store the AMIs being exported into kubevirt")
 	flag.Parse()
 
 	if amiId == "" {
 		log.Fatalf("--ami-id is required")
+	} else if s3Bucket == "" {
+		log.Fatalf("--s3-bucket is required")
 	}
 
 	cli, err := NewClient(region)
@@ -252,4 +376,34 @@ func main() {
 	// Step 3: Export AMI to s3 bucket
 	// ----------------
 
+	exists, err := cli.IsImageExported(s3Bucket, s3Prefix, amiToExport, ExportImageFormat)
+	if err != nil {
+		log.Fatalf("Error encountered determining if image is exported to s3 bucket: %v", err)
+	}
+
+	// TODO
+	// - Determine if task for ami already exists or not
+	// - If task already exists, is it completed?
+	// - If it is completed, does s3 export file still exist?
+	//
+	// If all of that is true, then skip the export step
+	//
+	//
+
+	if !exists {
+		log.Printf("Exporting ami %s to s3 bucket %s", amiToExport, s3Bucket)
+		/*
+			taskId, err := cli.ExportImage(amiToExport, s3Bucket, s3Prefix, exportImageFormat)
+			if err != nil {
+				log.Fatalf("Creation of export task for AMI %s to s3 failed: %v", amiToExport, err)
+			}
+
+			err = cli.WaitForExportImageCompletion(taskId, time.Minute*15)
+			if err != nil {
+				log.Fatalf("Exporting of AMI %s to s3 failed: %v", amiToExport, err)
+			}
+		*/
+	} else {
+		log.Printf("Found existing s3 export for ami %s", amiToExport)
+	}
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/aws/aws-sdk-go-v2/config v1.6.1
 	github.com/aws/aws-sdk-go-v2/service/ec2 v1.14.0
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.13.0
 	github.com/aws/aws-sdk-go-v2/service/sts v1.6.2
 )
